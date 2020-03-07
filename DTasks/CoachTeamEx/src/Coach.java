e4b8c4b1f106bbd7af16eb74df602fc98e8adf50
rgkog <rgkog@DESKTOP-GI2KK2V>
2020-03-07 14:52:45 +0200

    test1

---
+++ DTasks/CoachTeamEx/src/Coach.java
@@ -0,0 +1,41 @@
+
+public class Coach {
+	private String name;
+	private int yearsOfExperince;
+	private Team theTeam;
+//============================================================================	
+	public Coach(String name,int yearsOfExperince,Team theTeam) {
+		this.setName(name);
+		this.yearsOfExperince=yearsOfExperince;
+		setTeam(theTeam);
+	}
+	public Coach(String name,int yearsOfExperince) {
+		this(name, yearsOfExperince, null);
+	}
+	
+//============================================================================	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setTeam(Team newTeam) {
+		
+		if(theTeam!=newTeam) {
+			theTeam=newTeam;
+			if(theTeam!=null) {
+				theTeam.setCoach(this);
+			}
+		}
+	}	
+//============================================================================		
+	public void show() {
+		System.out.print(name+"'s experince is " +yearsOfExperince+" years, and he ");
+		if(theTeam==null)
+			System.out.println(" do not coach any team now.");
+		else
+			System.out.print(" coaches the team " );
+	}
+	
+}
