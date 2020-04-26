e89b4cffe91f0389b2c9d7762e05a6de56cc0e66
rgkog <rgkog@DESKTOP-GI2KK2V>
2020-04-26 19:03:14 +0300

    hii

--- HW2020/hw2/src/components/Junction.java
+++ HW2020/hw2/src/components/Junction.java
@@ -3,7 +3,9 @@ package components;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Junction  /*extends Point*/ /*implements RouteParts*/  {
+import utilities.Point;
+
+public class Junction  extends Point /*implements RouteParts*/  {
 	private int objectsCount=1;
 	
 	private ArrayList<Road> enteringRoads;
@@ -12,10 +14,11 @@ public class Junction  /*extends Point*/ /*implements RouteParts*/  {
 	//================================
 	
 	public Junction() {
-		//this();
-		//this.setJunctionName(String.valueOf(junctionName));
-		//setX(new Random().nextInt(799) + 0 );
-		//setY(new Random().nextInt(599) + 0);
+		this();
+		this.setJunctionName(String.valueOf(junctionName));
+		setX(new Random().nextInt(799) + 0 );
+		setY(new Random().nextInt(599) + 0);
+		
 		
 	}
 	
@@ -37,6 +40,54 @@ public class Junction  /*extends Point*/ /*implements RouteParts*/  {
 	}
 	public void setJunctionName(String junctionName) {
 		this.junctionName = junctionName;
+	}
+
+	@Override
+	public boolean checkValue(double Val, double min, double max) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void correctingMessage(double wrongVal, double correctVal, String varName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void errorMessage(double wrongVal, String varName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean getRandomBoolean() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getRandomDouble(double min, double max) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getRandomInt(int min, int max) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Integer> getRandomIntArray(int min, int max, int arraySize) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void successMessage(String objName) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
