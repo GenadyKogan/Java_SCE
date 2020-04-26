6dee0a383da3945a8b12a0e5871c9e8b3e875503
rgkog <rgkog@DESKTOP-GI2KK2V>
2020-04-26 23:13:56 +0300

    RouteParts

---
+++ HW2020/hw2/src/utilities/RouteParts.java
@@ -0,0 +1,10 @@
+package utilities;
+
+public interface RouteParts extends Utilities{
+	double calcEstimatedTime(Object obj);
+	//boolean canLeave(Vehicle vehicle);
+	//void checkIn(Vehicle vehicle);
+	//void checkout(Vehicle vehicle);
+	//RouteParts findNextPart(Vehicle vehicle);
+	//void stayOnCurrentPart(Vehicle vehicle); 
+}
