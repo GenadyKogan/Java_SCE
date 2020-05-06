b1fc013d73c2bace243d366ed01ad749f83429ae
sapir52 <sapirbn93@gmail.com>
2020-05-06 20:05:35 +0300

    update2345

--- HW2020/hw2/src/components/RandomTrafficLights.java
+++ HW2020/hw2/src/components/RandomTrafficLights.java
@@ -12,56 +12,6 @@ public class RandomTrafficLights extends TrafficLights{
 	public void changeIndex(){
 		this.setGreenLightIndex(getRandomInt(0, this.getRoads().size()));
 	}
-	//================================
-	@Override
-	public boolean checkValue(double Val, double min, double max) {
-		if(Val<max && Val>min)
-			return true;
-		return false;
-	}
-	@Override
-	public void correctingMessage(double wrongVal, double correctVal, String varName) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void errorMessage(double wrongVal, String varName) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public boolean getRandomBoolean() {
-		boolean [] booleanElem={true,false};
-		return booleanElem[ new Random().nextInt(booleanElem.length)];
-	}
-
-	@Override
-	public double getRandomDouble(double min, double max) {
-		 double random_double = Math.random() * (max - min + 1) + min; 
-		 return random_double;
-	}
-
-
-	@Override
-	public int getRandomInt(int min, int max) {
-		Random random = new Random();
-		return random.nextInt(max-min+1) + min;
-	}
-
-	@Override
-	public ArrayList<Integer> getRandomIntArray(int min, int max, int arraySize) {
-		ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i <arraySize; i++) {
-            list.add(getRandomInt(min,max));
-        }
-        return list;
-	}
-	@Override
-	public void successMessage(String objName) {
-		System.out.println(objName+"has been created\n");
-
-		
-	}
 
 
 }
