8dd80f4c04f69b2334f2cf8de23d1707bc10572a
sapir52 <sapirbn93@gmail.com>
2020-05-19 02:16:45 +0300

    a

--- HW2020/HW2_final_solution/src/gui/RoadFrame.java
+++ HW2020/HW2_final_solution/src/gui/RoadFrame.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
+//
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
