4435c81cd69fe012dcd3c262b695286300333a72
rgkog <rgkog@DESKTOP-GI2KK2V>
2020-06-19 13:27:59 +0300

    //

--- Design Patern/Abstract Factory Method/src/AbstractFactoryPatternDemo.java
+++ Design Patern/Abstract Factory Method/src/AbstractFactoryPatternDemo.java
@@ -2,7 +2,7 @@ public class AbstractFactoryPatternDemo {
 	   public static void main(String[] args) { 
 		      //get shape factory 
 		      AbstractFactory shapeFactory = FactoryProducer.getFactory("SHAPE"); 
-		 
+		 //
 		      //get an object of Shape Circle 
 		      Shape shape1 = shapeFactory.getShape("CIRCLE"); 
 		 
