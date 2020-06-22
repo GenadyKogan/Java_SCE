5614845f841bae01acb1dea677d90df506c7ef1b
rgkog <rgkog@DESKTOP-GI2KK2V>
2020-06-22 15:50:33 +0300

    Decorator

---
+++ Design Patern/Decorator/src/RedShapeDecorator.java
@@ -0,0 +1,17 @@
+
+public class RedShapeDecorator extends ShapeDecorator implements Shape { 
+		 
+		   public RedShapeDecorator(Shape decoratedShape) { 
+		      super(decoratedShape);   
+		   } 
+		 
+		   @Override 
+		   public void draw() { 
+		      decoratedShape.draw();         
+		      setRedBorder(decoratedShape); 
+		   } 
+		 
+		   private void setRedBorder(Shape decoratedShape){ 
+		      System.out.println("Border Color: Red"); 
+		   }
+}
